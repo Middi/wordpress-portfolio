0363c38b4ca16e87d6e3dae3b2f134b61a846a3a
“Richard Middleton” <Richard@richardmiddleton.me>
2017-08-16 00:52:07 +0200

    spaces from page-home

--- page-home.php
+++ page-home.php
@@ -8,25 +8,20 @@ define( 'WP_USE_THEMES', false ); get_header(); ?>
 
 
 <main>
-
+    
         <!--====== Jumbotron =======-->
-
         <?php get_template_part( 'template-parts/content', 'hero' ); ?>
 
          <!--====== Portfolio =======-->
-        
         <?php get_template_part( 'template-parts/content', 'portfolio' ); ?>
 
          <!--====== About =======-->
-        
         <?php get_template_part( 'template-parts/content', 'about' ); ?>
 
          <!--====== Skills =======-->
-        
         <?php get_template_part( 'template-parts/content', 'skills' ); ?>
 
          <!--====== Contact =======-->
-
         <?php get_template_part( 'template-parts/content', 'contact' ); ?>
 
 </main>
