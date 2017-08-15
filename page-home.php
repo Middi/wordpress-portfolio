688a41412028a687277ba19c18defd0ba052cfb7
“Richard Middleton” <Richard@richardmiddleton.me>
2017-08-16 00:15:54 +0200

    skills slider now dynamic

--- page-home.php
+++ page-home.php
@@ -109,57 +109,23 @@ define( 'WP_USE_THEMES', false ); get_header(); ?>
                 <div id="skills-slider">
                     <ul class="slides">
 
-                        <li tooltip="HTML5" class="slide">
-                            <i class="devicon-html5-plain-wordmark colored"></i>
+                    <?php $loop = new WP_Query( array( 'post_type' => 'skills_slider', 'posts_per_page' => '-1', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
+
+                    <?php while($loop->have_posts()) : $loop->the_post(); ?>
+
+                        <li tooltip="<?php the_title(); ?>" class="slide">
+                            <i class="<?php the_field('skill') ?>"></i>
                         </li>
                         
-                        <li tooltip="CSS3" class="slide">
-                            <i class="devicon-css3-plain-wordmark colored"></i>
-                        </li>
-
-                        <li tooltip="Javascript" class="slide">
-                            <i class="devicon-javascript-plain colored"></i>
-                        </li>
-
-                        <li tooltip="jQuery" class="slide">
-                            <i class="devicon-jquery-plain-wordmark colored"></i>
-                        </li>
-
-                        <li tooltip="PHP" class="slide">
-                            <i class="devicon-php-plain colored"></i>
-                        </li>
-
-                        <li tooltip="Bootstrap" class="slide">
-                            <i class="devicon-bootstrap-plain devicon colored"></i>
-                        </li>
-
-                        <li tooltip="Photoshop" class="slide">
-                            <i class="devicon-photoshop-plain colored"></i>
-                        </li>
-
-                        <li tooltip="GitHub" class="slide">
-                            <i class="devicon-github-plain colored"></i>
-                        </li>
-
-                        <li tooltip="Sass" class="slide">
-                            <i class="devicon-sass-original devicon colored"></i>
-                        </li>
-                        
-                        <li tooltip="Node.js" class="slide">
-                            <i class="devicon-nodejs-plain colored"></i>
-                        </li>
-
-                        <li tooltip="MongoDB" class="slide">
-                            <i class="devicon-mongodb-plain colored"></i>
-                        </li>
-
-                        <li tooltip="Sketch" class="slide">
+                    <?php endwhile; ?>
+                                    
+                    <li tooltip="Sketch" class="slide">
                             <svg xmlns="http://www.w3.org/2000/svg" width="58px" version="1.1" viewBox="0 0 26 24">
                                 <path fill-rule="evenodd" fill="#EAAC29" d="m22.317 1.9817l3.683 4.2935-13 17.725-13-17.725 3.6833-4.2933 4.6584-1.9817h9.3163l4.659 1.9817zm-13.217 15.258l-3.4667-6.363-2.73-2.0697 6.1967 8.4327zm10.162-6.231h-12.567l6.283 11.516 6.284-11.516zm-2.405 6.231l3.466-6.363 2.73-2.0697-6.196 8.4327zm-11.029-7.3097l-4.745-3.611 2.8817-3.3688 4.095 2.0036-2.2317 4.9762zm3.0334-4.6459l-2.1667 4.8436h12.567l-2.167-4.8436h-8.2333zm11.266 4.6459l4.745-3.611-2.881-3.3688-4.095 2.0036 2.231 4.9762zm-3.033-5.5266h-8.2333l-3.9867-1.9817 3.6183-1.5413 8.9697 0.00003 3.619 1.5413-3.987 1.9817z"
                                 />
                             </svg>
                         </li>
-
+                        
                     </ul>
                 </div>
 
