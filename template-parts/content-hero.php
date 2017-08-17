e8c112586a32ae6ace196267de5a868d3e611410
“Richard Middleton” <Richard@richardmiddleton.me>
2017-08-17 18:49:00 +0200

    hero image now dynamic

--- template-parts/content-hero.php
+++ template-parts/content-hero.php
@@ -1,4 +1,5 @@
-<section id="hero">
+<section id="hero" style="background-image: linear-gradient(to bottom, rgba(59, 146, 248, 0.7), rgba(59, 146, 248, 0.7)), url('<?php the_field('hero_image'); ?>')">
+
     <div class="container">
         <div class="text-content">
             <img src="<?php bloginfo('stylesheet_directory'); ?>/images/avatar.png" alt="">
